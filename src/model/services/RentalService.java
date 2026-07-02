db695c8e2101af1233aaa60ed01f2a42dad07269
unknown <felcacontatobr@gmail.com>
2026-07-02 02:24:47 -0300

    with interface

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -7,20 +7,20 @@ public class RentalService {
 
 	private Double pricePerDay;
 	private Double pricePerHour;
-	
-	private BrazilTaxService taxService;
 
-	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+	private TaxService taxService;
+
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
 		this.pricePerDay = pricePerDay;
 		this.pricePerHour = pricePerHour;
 		this.taxService = taxService;
 	}
-	
+
 	public void processInvoice(CarRental carRental) {
 		long t1 = carRental.getStart().getTime();
 		long t2 = carRental.getFinish().getTime();
 		double hours = (double)(t2 - t1) / 1000 / 60 / 60;
-		
+
 		double basicPayment;
 		if (hours <= 12.0) {
 			basicPayment = pricePerHour * Math.ceil(hours);
